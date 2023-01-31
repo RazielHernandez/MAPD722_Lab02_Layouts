921a7bd403c500fed2f670ff425c9e70c9b1de92
RazielHernandez <razo.puma.azteca@gmail.com>
2023-01-30 22:25:05 -0500

    Second commit. Create the rest of the widgets

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -34,13 +34,13 @@ class Home extends StatelessWidget {
               child: Column(         
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  //_buildJournalEntry(),
+                  _buildJournalEntry(),
                   Divider(),
-                  //_buildJournalWeather(),
+                  _buildJournalWeather(),
                   Divider(),
-                  //_buildJournalTags(),
+                  _buildJournalTags(),
                   Divider(),
-                  //_buildJournalFooterImages(),
+                  _buildJournalFooterImages(),
                 ],
               ),
             ),
@@ -52,8 +52,123 @@ class Home extends StatelessWidget {
 }
 
 Image _buildJournalHeaderImage() {
-   return Image(
+  return Image(
     image: AssetImage('assets/images/present.jpg'),
     fit: BoxFit.cover,
-   );
-  }
+  );
+}
+
+Column _buildJournalEntry() {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        'My Birthday',
+        style: TextStyle(
+          fontSize: 32.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      Divider(),
+      Text(
+        'It’s going to be a great birthday. We are going out for dinner at my favorite place, then watch a movie after we go to the gelateria for ice cream and espresso.',
+        style: TextStyle(color: Colors.black54),
+      ),
+    ],
+  );
+}
+
+Row _buildJournalWeather() {
+  return Row(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Icon(
+            Icons.wb_sunny,
+            color: Colors.orange,
+          ),
+        ],
+      ),
+      SizedBox(width: 16.0,),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Text(
+                '81º Clear',
+                style: TextStyle(color: Colors.deepOrange),
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Text(
+                '4500 San Alpho Drive, Dallas, TX United States',
+                style: TextStyle(color: Colors.grey),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ],
+  );
+}
+
+Wrap _buildJournalTags() {
+  return Wrap(
+    spacing: 8.0,
+    children: List.generate(7, (int index) {
+      return Chip(
+        label: Text(
+          'Gift ${index + 1}',
+          style: TextStyle(fontSize: 10.0),
+        ),
+        avatar: Icon(
+          Icons.card_giftcard,
+          color: Colors.blue.shade300,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4.0),
+          side: BorderSide(color: Colors.grey),
+        ),
+        backgroundColor: Colors.grey.shade100,
+      );
+    }),
+  );
+}
+
+Row _buildJournalFooterImages() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      CircleAvatar(
+        backgroundImage: AssetImage('assets/images/salmon.jpg'),
+        radius: 40.0,
+      ),
+      CircleAvatar(
+        backgroundImage: AssetImage('assets/images/asparagus.jpg'),
+        radius: 40.0,
+      ),
+      CircleAvatar(
+        backgroundImage: AssetImage('assets/images/strawberries.jpg'),
+        radius: 40.0,
+      ),
+      SizedBox(
+        width: 100.0,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            Icon(Icons.cake),
+            Icon(Icons.star_border),
+            Icon(Icons.music_note),
+            Icon(Icons.movie),
+          ],
+        ),
+      ),
+    ],
+  );
+}
